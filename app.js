bd41d5578ed8dc79bf442ad1054b82c0e18a21c6
Todd Hintzmann <todd@hintzmann.net>
2026-08-30 08:06:31 -0400

    trace renderMasterTable callsite

--- app.js
+++ app.js
@@ -579,6 +579,8 @@ let _rc = 0; function recomputeTotals() {
 }
 let _rmt = 0; function renderMasterTable() {
   _rmt++;
+  console.log("renderMasterTable call #" + _rmt + " from " + (new Error()).stack.split("
+")[2]);
   if (_rmt > 1) console.warn('renderMasterTable called ' + _rmt + ' times');
   const tbody = $("#m-table-body");
   if (!tbody) return;
